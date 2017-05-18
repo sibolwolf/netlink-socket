46d0b6d3515782ef9684feb3bad21a1249c19a6c
hejinlai <hejinlai@foxmail.com>
2017-05-18 17:56:19 +0800

    v2.0
    Change-Id: I16c2db0ed7565e7ad5cde7302e3da6b966a87157

--- github.com/sibolwolf/netlink-socket/netlink-socket.go
+++ github.com/sibolwolf/netlink-socket/netlink-socket.go
@@ -2,12 +2,16 @@ package main
 
 import (
     "fmt"
+    "os"
     "golang.org/x/sys/unix"
 )
 
+
+
 func main() {
     // To communicate with netlink, a netlink socket must be opened. This is done using the socket() system call:
-    fd, err := unix.Socket(
+    fmt.Println("Hello, this is netlink socket")
+    fd, socket_err := unix.Socket(
         // Always used when opening netlink sockets.
         unix.AF_NETLINK,
         // Seemingly used interchangeably with SOCK_DGRAM,
@@ -18,35 +22,49 @@ func main() {
         unix.NETLINK_KOBJECT_UEVENT,
     )
 
+    fmt.Println("unix.NETLINK_KOBJECT_UEVENT:", unix.NETLINK_KOBJECT_UEVENT)
+    fmt.Println("fd is:", fd)
+
+    if socket_err != nil {
+        fmt.Println("Socket_err is:" + socket_err.Error())
+    }
+
     // Once the socket is created, bind() must be called to prepare it to send and receive messages.
-    err := unix.Bind(fd, &unix.SockaddrNetlink{
+    bind_err := unix.Bind(fd, &unix.SockaddrNetlink{
         // Always used when binding netlink sockets.
         Family: unix.AF_NETLINK,
         // A bitmask of multicast groups to join on bind.
         // Typically set to zero.
-        Groups: 0,
+        Groups: 0xffffffff,
         // If you'd like, you can assign a PID for this socket
         // here, but in my experience, it's easier to leave
         // this set to zero and let netlink assign and manage
         // PIDs on its own.
-        Pid: 0,
+        Pid: uint32(os.Getpid()),
     })
+
+    if bind_err != nil {
+        fmt.Println("Bind_err is:" + bind_err.Error())
+    }
+
+    bstore := make([]byte, os.Getpagesize())
+    fmt.Println("length b is:", len(bstore))
     for {
-        b := make([]byte, os.Getpagesize())
+        fmt.Println("Start reading2 ...")
         for {
             // Peek at the buffer to see how many bytes are available.
-            n, _, _ := unix.Recvfrom(fd, b, unix.MSG_PEEK)
-            fmt.Println(n)
+            n, _, _ := unix.Recvfrom(fd, bstore, 4096)
+            fmt.Println("Length of data is:", n)
+            // http://stackoverflow.com/questions/14230145/what-is-the-best-way-to-convert-byte-array-to-string
+            fmt.Println(string(bstore[:n]))
             // Break when we can read all messages.
-            if n < len(b) {
+            if n < len(bstore) {
                 break
             }
             // Double in size if not enough bytes.
-            b = make([]byte, len(b)*2)
+            bstore = make([]byte, len(bstore)*2)
         }
-        // Read out all available messages.
-        n, _, _ := unix.Recvfrom(fd, b, 0)
-        fmt.Println(n)
+        fmt.Println("Start reading3 ...")
     }
 
 }
